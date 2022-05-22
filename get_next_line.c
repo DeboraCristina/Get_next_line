5986e920a61ad8ce85b6093e501e2af2f9c70a3c
DeboraCristina <deboracristinaproficional1@gmail.com>
2022-05-22 12:48:43 +0200

    fix - print null lines

--- get_next_line.c
+++ get_next_line.c
@@ -1,15 +1,4 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: desilva <dede-2231@hotmail.com>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/22 01:28:04 by desilva           #+#    #+#             */
-/*   Updated: 2022/05/22 01:29:21 by desilva          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
+# include <stdio.h>
 #include "get_next_line.h"
 
 char	*ft_readline(char *content)
@@ -17,9 +6,13 @@ char	*ft_readline(char *content)
 	char	*line;
 	int		i;
 
+	if (!content)
+		return (0);
 	i = 0;
 	while (content[i] && content[i] != '\n')
 		i++;
+	if (!i)
+		return (0);
 	i += 1;
 	line = (char *) malloc(sizeof(char) * i);
 	if (!line)
@@ -39,14 +32,17 @@ char	*get_next_line(int fd)
 	char	buff[BUFFER_SIZE];
 	char	*content;
 	char	*line;
-	size_t	size;
+	int		size;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
 	size = 1;
+	content = 0;
 	while (size && !ft_strchr(buff, '\n'))
 	{
 		size = read(fd, buff, BUFFER_SIZE);
+		if (size < 0)
+			return (0);
 		buff[size] = 0;
 		content = ft_strjoin(content, buff);
 		if (!content)
@@ -60,5 +56,7 @@ char	*get_next_line(int fd)
 		return (0);
 	}
 	content = ft_strchr(content, '\n');
+	printf("\033[1;95mline\n%s\033[0mEND\n", line);
+	printf("\033[1;94mcontent\n%s\033[0mEND\n", content);
 	return (line);
 }
