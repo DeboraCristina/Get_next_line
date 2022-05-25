4da8fbb5156d426be0fc54403a6773735e6a0ae4
DeboraCristina <deboracristinaproficional1@gmail.com>
2022-05-25 09:56:07 +0200

    Re-init project

---
+++ get_next_line.c
@@ -0,0 +1,12 @@
+# include "debug.h"
+#include "get_next_line.h"
+
+// Function 01
+// Function 02
+// Function 03
+// Function 04
+// Function 05
+char	*get_next_line(int fd)
+{
+	return (line);
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,8 @@
+# include "debug.h"
+#include "get_next_line.h"
+
+// Function 01
+// Function 02
+// Function 03
+// Function 04
+// Function 05
