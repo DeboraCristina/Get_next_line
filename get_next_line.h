d2973ee43accabe54eec0a798ae92e14d70c9a00
DeboraCristina <deboracristinaproficional1@gmail.com>
2022-05-25 11:56:51 +0200

    add utils functions

--- get_next_line.h
+++ get_next_line.h
@@ -1,6 +1,16 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <unistd.h>
+# include <stdlib.h>
+
 char	*get_next_line(int fd);
+
+// === utils === //
+size_t	ft_strlen(const char *str);
+
+char	*ft_strchr(const char *s, int c);
+
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -2,7 +2,67 @@
 #include "get_next_line.h"
 
 // Function 01
+size_t	ft_strlen(const char *str)
+{
+	size_t	c;
+
+	c = 0;
+	if (str)
+	{
+		while (str[c])
+			c++;
+	}
+	return (c);
+}
+
 // Function 02
+char	*ft_strchr(const char *s, int c)
+{
+	int				i;
+	unsigned char	l;
+
+	i = 0;
+	l = c;
+	while (s[i])
+	{
+		if (l == s[i])
+			return ((char *) s + i);
+		i++;
+	}
+	if (!l)
+		return ((char *) s + i);
+	return (0);
+}
+
 // Function 03
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*sj;
+	int		count;
+
+	sj = (char *) malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	count = 0;
+	if (!sj)
+		return (0);
+	if (s1)
+	{
+		printf("AKI1\n");
+		while (*s1)
+			sj[count++] = *s1++;
+		printf("AKI2\n");
+		free((void *) s1);
+	}
+	if (s2)
+	{
+		printf("AKI3\n");
+		while (*s2)
+			sj[count++] = *s2++;
+		printf("AKI4\n");
+		// free((void *) s2);
+	}
+	sj[count] = 0;
+	return (sj);
+}
+
 // Function 04
 // Function 05
