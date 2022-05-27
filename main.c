a7f155b67f108b05f236a8c1fc7cae95749860ad
DeboraCristina <deboracristinaproficional1@gmail.com>
2022-05-27 09:14:28 +0200

    testers

--- main.c
+++ main.c
@@ -9,23 +9,43 @@ int	main(void)
 	int		fileDescriptor;
 
 	int		timeout;
+	int		test;;
 	char	*file;
 
 	line = "a";
+	test = 1; // 1 - 4
 
-	timeout = 7;
-	file = "files/little";
-	// timeout = 25;
-	// file = "files/medium";
-	// timeout = 250;
-	// file = "files/bigger";
-	// file = "get_next_line.h";
+	switch (test)
+	{
+		case 1:
+			timeout = 15;
+			file = "files/little";
+			break ;
+		case 2:
+			timeout = 25;
+			file = "files/medium";
+			break ;
+		case 3:
+			timeout = 250;
+			file = "files/bigger";
+			break ;
+		case 4:
+			timeout = 40;
+			file = "get_next_line.h";
+			break ;
+		default:
+			dprint("ERRO", RED);
+	}
 
 	fileDescriptor = open(file, O_RDONLY);
 
 	while (line && timeout)
-		{
+	{
 		line = get_next_line(fileDescriptor);
+		if (line)
+			printf("%s", line);
+		else
+			dprint("NULL", PURPLE);
 		free(line);
 		timeout--;
 	}
